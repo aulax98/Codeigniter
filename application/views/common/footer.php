488527497400f32d0434120927d7cd220cb81a45
unknown <alain.danten@yahoo.com>
2020-11-19 14:04:26 +0100

    Fourth commit

---
+++ application/views/common/footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <hr>
+    <footer>
+        <p>&copy; Alain DANTEN 2020</p>
+    </footer>
+</div>
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
