54da1ff9cf40a0ed547d3e965e85bc2e0319897a
unknown <alain.danten@yahoo.com>
2020-11-23 10:27:11 +0100

    Tenth commit

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -27,7 +27,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <p class="navbar-brand">Alain Danten</>
+        <a href="index.html" class="navbar-brand">Alain Danten</a>
       </div>
       <div class="collapse navbar-collapse" id="main_nav">
         <ul class="nav navbar-nav">
